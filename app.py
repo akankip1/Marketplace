bfdb3f7d9b4a140bade53d4e78b4de4032e3d43e
Ashrit Kankipati <venkat.ashrit@gmail.com>
2023-12-19 05:20:12 -0500

    refactor

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+# Load variables from .env file into environment
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    # Get environment variables or default values
+    flask_debug = os.getenv('FLASK_DEBUG', default='0')
+    # Convert string to boolean for debug
+    debug = flask_debug.lower() in ['true', '1', 'yes']
+
+    # Run the app with debug mode based on environment variable
+    app.run(debug=debug)
